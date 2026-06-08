a80395ec95351cb5506ee4d47d40767901321772
luis <luiinge@gmail.com>
2026-06-08 18:58:40 +0200

    fix products config

--- products/src/main/java/com/example/products/infra/persistence/entities/ProductEntity.java
+++ products/src/main/java/com/example/products/infra/persistence/entities/ProductEntity.java
@@ -1,11 +1,7 @@
 package com.example.products.infra.persistence.entities;
 
 import java.math.BigDecimal;
-import java.time.Instant;
 
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
